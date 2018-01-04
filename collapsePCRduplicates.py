e25a4c93f8d0ebee57343f41f3ddf89adad8febe
garrett-parker <garrett.parker@me.com>
2018-01-04 14:15:24 -0800

    Update collapsePCRduplicates.py

--- collapsePCRduplicates.py
+++ collapsePCRduplicates.py
@@ -8,7 +8,6 @@ import argparse
 import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 import matplotlib.pyplot as plt
-import pdb
 
 
 #updated 09/04/2015
@@ -21,19 +20,30 @@ def editDistance(s1,s2):
             k+=1
     return k
 
+def addUMI():
+    if args.e:
+        i = 1
+    else:
+        i = 3
+    new = read.query_name.split("_")[i] + "_" + str(read.query_sequence)
+    return new
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument('inbam', help='Mapped reads in bam format')
 ap.add_argument('outbam', help='Output file to save reads after collapsing PCR duplicates')
-#ap.add_argument('--testN', type=int,
-#                help='Run a test using only the first N features, and then '
-#                'print out some example feature IDs and their attributes')
-ap.add_argument('--m', action='store_true',help='Save multi-mapped reads')
-ap.add_argument('--c', action='store_true',help='Change chromosome format')
+ap.add_argument("--MAPQ",help="mapping quality to extract uniquely-mapped reads. Only reads with this MAPQ will be selected. By default all reads will be considered", default=60,type=int)
+ap.add_argument("--u",help="a binary flag used to indicate that only uniquely mapped reads will be considered. By default uniquely mapped reads are defined as reads with MAPQ=60",action="store_true")
+ap.add_argument("--chr",help="Number of autosomes. By default 20", default=20,type=int)
+ap.add_argument("--end",help="a binary flag used to indicate that end position is considered. This is beta version use with caution",action="store_true")
+ap.add_argument('--e', action='store_true',help='UMI-tools example format')
 
-#cmd https://gist.github.com/daler/ec481811a44b3aa469f3
+
 
 args = ap.parse_args()
+
+
+
 
 
 
@@ -46,7 +56,7 @@ out=args.outbam
 
 chr_list=[]
 
-if args.c:
+if args.e:
     for i in range(1,20):
         chr_list.append('chr'+str(i))
 
@@ -55,14 +65,14 @@ if args.c:
     chr_list.append('chrM')
 
 else:
-    for i in range(1,20):
+    for i in range(1,args.chr):
         chr_list.append(str(i))
 
     chr_list.append('X')
     chr_list.append('Y')
     chr_list.append('MT')
 
-#print chr
+print (chr)
 
 position=[]
 position_all_uniq=[]
@@ -103,71 +113,62 @@ outfile = pysam.AlignmentFile(out, "wb", header=bam_header)
 
 
 
-print "Open ",bam, "via pysam"
+print ("Open ",bam, "via pysam")
+
 
 
 
 for chr in chr_list:
     dict.clear()
     position[:]=[]
-
-    if args.c:
+    if args.e:
         print "----------",chr
     else:
-        print "----------chr",chr
+        print ("----------chr",chr)
     for read in samfile.fetch(chr):
-        #stores the name of the read in mappedReads
         mappedReads.append(read.query_name)
-        if args.m:
-            if read.mapq==50:
-                numberReadsUnique+=1
+
+        if args.u:
+            if read.mapq==args.MAPQ:
+                numberReadsUnique += 1
+                position.append(read.reference_start)
+                readLength.append(len(read.query_sequence))
+        else:
             numberReadsUniquePlusMultiMapped+=1
             position.append(read.reference_start)
-            #stores length of read
-            #returns error with umi-tools example.bam
             readLength.append(len(str(read.query_sequence)))
-        else:
-            #checks that the mapping quality of the read is equal to 50
-            if read.mapq>=10:
-                #increments numberReadsUnique
-                numberReadsUnique+=1
-                #stores the start position of the read
-                position.append(read.reference_start)
-                #stores the length of the read
-                #returns error with umi-tools data
-                readLength.append(len(str(read.query_sequence)))
 
 
-    print "numberReadsUnique: ",numberReadsUnique
-    print "numberReadsUniquePlusMultiMapped: ",numberReadsUniquePlusMultiMapped
-    print "mappedReads length: ",len(mappedReads)
 
-    #creates dictionary consisting of read position values
+    print ("numberReadsUnique",numberReadsUnique)
+    print ("numberReadsUniquePlusMultiMapped",numberReadsUniquePlusMultiMapped)
+
+
+
     counter_chr=collections.Counter(position)
     position_all_uniq+=position
-    print "Number of read positions stored: ", len(position)
+    print ("Number of position with #reads staring >=1", len(position))
 
     count=0
 
-    print  "Processing", len(counter_chr.items()), "items"
+    print  ("Processing", len(counter_chr.items()), "items")
 
     for key,val in counter_chr.items():
-        #print key,val
         if count%10000==1:
-            print count
+            print (count)
         count+=1
-
+        
         if val==1:
+
             for read in samfile.fetch(chr,key,key+1):
                 if read.reference_start==key:
+
                     outfile.write(read)
-                    #adds read length to readLength_filtered
-                    #returns error with umi-tools data
                     readLength_filtered.append(len(str(read.query_sequence)))
                     numberReadsUnique_filtered+=1
                     readSet.add(read.query_name)
-
-
+     
+    
 
 
 
@@ -175,50 +176,82 @@ for chr in chr_list:
         elif val>1:
             setReads=set()
             setReads.clear()
-
+    
             if val>1000:
-                print val, chr,key
+                print (val, chr,key)
             Read=[]
             Read[:]=[]
             for read in samfile.fetch(chr,key,key+1):
                 if read.reference_start==key:
                     Read.append(read)
-                    #Identify UMI
-                    if args.c:
-                        setReads.add(read.query_name.split("_")[1]+"_"+str(read.query_sequence))
+                    if args.end:
+                        #UMI-tools format
+                        if args.e:
+                            setReads.add(addUMI())
+                        #default format
+                        else:
+                            setReads.add(read.query_name.split("_")[3] + "_" + read.query_sequence)
                     else:
-                        setReads.add(read.query_name.split("_")[3]+"_"+str(read.query_sequence))
+                        #UMI-tools format
+                        if args.e:
+                            setReads.add(read.query_name.split("_")[1]+"_"+str(read.query_sequence))
+                        #default format
+                        else:
+                            setReads.add(read.query_name.split("_")[3]+"_"+read.query_sequence+"_"+str(read.reference_end))
 
-        #print key,val
 
+                    #print (read.query_sequence,read.query_name,read.query_name.split("_")[3],read.reference_start,read.reference_end)
 
-
-
+            
 
             notsetReads=set()
             notsetReads.clear()
             numberReadsUnique_covGreated1+=len(setReads)
             for i in range(0,val):
-                if args.c:
-                    if Read[i].query_name.split("_")[1]+"_"+str(Read[i].query_sequence) in setReads and Read[i].query_name.split("_")[1]+"_"+str(Read[i].query_sequence) not in notsetReads:
-                            outfile.write(Read[i])
-                            numberReadsUnique_filtered+=1
-                            readLength_filtered.append(len(str(Read[i].query_sequence)))
-                            notsetReads.add(Read[i].query_name.split("_")[1]+"_"+str(Read[i].query_sequence))
-                            readSet.add(Read[i].query_name)
+                if args.end:
+                    #UMI-tools format
+                    if args.e:
+                        extended_read_name = Read[i].query_name.split("_")[1] + "_" + str(Read[i].query_sequence)+"_"+str(Read[i].reference_end)
+                    #default format
+                    else:
+                        extended_read_name = Read[i].query_name.split("_")[3] + "_" + Read[i].query_sequence+"_"+str(Read[i].reference_end)
                 else:
-                    if Read[i].query_name.split("_")[3]+"_"+str(Read[i].query_sequence) in setReads and Read[i].query_name.split("_")[3]+"_"+str(Read[i].query_sequence) not in notsetReads:
-                            outfile.write(Read[i])
-                            numberReadsUnique_filtered+=1
-                            readLength_filtered.append(len(str(Read[i].query_sequence)))
-                            notsetReads.add(Read[i].query_name.split("_")[3]+"_"+str(Read[i].query_sequence))
-                            readSet.add(Read[i].query_name)
+                    #UMI-tools format
+                    if args.e:
+                        extended_read_name=Read[i].query_name.split("_")[1]+"_"+str(Read[i].query_sequence)
+                    #default format
+                    else:
+                        extended_read_name=Read[i].query_name.split("_")[3]+"_"+Read[i].query_sequence
+
+                if extended_read_name in setReads and extended_read_name not in notsetReads:
+                        outfile.write(Read[i])
+                        numberReadsUnique_filtered+=1
+                        readLength_filtered.append(len(str(Read[i].query_sequence)))
+
+
+                        if args.end:
+                            #UMI-tools format
+                            if args.e:
+                                notsetReads.add(Read[i].query_name.split("_")[1] + "_" + Read[i].query_sequence+"_"+str(Read[i].reference_end))
+                            #default format
+                            else:
+                                notsetReads.add(Read[i].query_name.split("_")[3] + "_" + Read[i].query_sequence+"_"+str(Read[i].reference_end))
+                        else:
+                            #UMI-tools format
+                            if args.e:
+                                notsetReads.add(Read[i].query_name.split("_")[1]+"_"+str(Read[i].query_sequence))
+                            #default format
+                            else:
+                                notsetReads.add(Read[i].query_name.split("_")[3]+"_"+Read[i].query_sequence)
+
+                        readSet.add(Read[i].query_name)
 
 
 
-
+                    
 
 outfile.close()
+
 
 
 
@@ -231,7 +264,6 @@ header=[]
 header.append('sample')
 header.append('Number of mapped reads')
 header.append('Number of reads mapped to unique location (UNIQUE reads)')
-header.append('Number of reads alligments after collapsing PCR dublicated (an aligment may include several copies of reads) ')
 header.append('Number of reads after collapsing PCR dublicated (each read is present once) ')
 
 
@@ -240,15 +272,13 @@ nr=[]
 nr.append(out.split('.')[0])
 nr.append(len(set(mappedReads)))
 nr.append(numberReadsUnique)
-
-
-nr.append(numberReadsUnique_filtered)
 nr.append(len(readSet))
 
 
 
 
 stat_f=out.split('.')[0]+'.number_of_reads_stat'
+print ("Save to ", stat_f)
 
 with open(stat_f, 'w') as fp:
     a = csv.writer(fp, delimiter=',')
@@ -260,7 +290,7 @@ with open(stat_f, 'w') as fp:
 
 counter=collections.Counter(position_all_uniq)
 position_all_uniq=set(position_all_uniq)
-print "Number of position with #reads staring >=1", len(position_all_uniq)
+print ("Number of position with #reads staring >=1", len(position_all_uniq))
 
 
 
@@ -273,14 +303,13 @@ xbins1=[]
 
 counter_length_filtered=collections.Counter(readLength_filtered)
 for key,val in counter_length_filtered.items():
-    #print key,val
     xbins1.append(val)
     x1.append(key)
 
 
 
 plot1=out.split('.')[0]+'.readLengthPCRDuplicatesCollapsed.png'
-print "save to",plot1
+print ("save to",plot1)
 
 
 
@@ -297,12 +326,11 @@ xbins2=[]
 
 counter_length=collections.Counter(readLength)
 for key,val in counter_length.items():
-    #print key,val
     xbins2.append(val)
     x2.append(key)
 
 plot2=out.split('.')[0]+'.readLengthBeforePCRduplicates.png'
-print "save to",plot2
+print ("save to",plot2)
 
 
 
@@ -311,7 +339,9 @@ plt.bar(x2,xbins2)
 plt.savefig(plot2)
 
 
+samfile.close()
 
 
+print ("DONE!")
 
-print "DONE!"
+
